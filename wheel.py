00b9345ec83b939178d5ebb156808be01d183761
zdeguzm53 <55883996+zdeguzm53@users.noreply.github.com>
2021-03-03 14:47:33 -0500

    Update and rename servo.py to wheel.py

--- wheel.py
+++ wheel.py
@@ -2,14 +2,16 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
-GPIO.setwarnings(False)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 p = GPIO.PWM(12, 100)
 p.start(5)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setwarnings(False)
 
 while True:
   if GPIO.input(10) == GPIO.HIGH
-         print("switch on")      
+         print("switch on")
+         GPIO.output(11, GPIO.HIGH)
 
   p.ChangeDutyCycle(2.5)
   time.sleep(5)
